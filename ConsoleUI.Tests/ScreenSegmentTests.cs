2503e70f8d88e4fc027a9b2bb479588e18c1c187
Kresten Laust <kressckerl@gmail.com>
2021-05-23 21:33:30 +0200

    Flere unit tests

---
+++ ConsoleUI.Tests/ScreenSegmentTests.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleUI.UI;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ConsoleUI.Tests
+{
+    [TestClass]
+    public class ScreenSegmentTests
+    {
+        [TestMethod]
+        public void TestReadScreenSegmentWith1DimensionalArray()
+        {
+            WinAPI.CharInfo[] charArray = new WinAPI.CharInfo[]
+            {
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'A' } },
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'B' } },
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'C' } }
+            };
+
+            // 1 Tall, 2 Wide, 1 offset-x, 0 offset-y
+            ScreenSegment segment = new ScreenSegment(charArray, charArray.Length, 1, 2, 1, 0);
+
+            // Antag første element af segmentet er lig andet element af den originale.
+            Assert.AreEqual(charArray[1], segment[0, 0]);
+        }
+
+        [TestMethod]
+        public void TestScreenSegmentBounds()
+        {
+            WinAPI.CharInfo[] charArray = new WinAPI.CharInfo[]
+            {
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'A' } },
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'B' } },
+                new WinAPI.CharInfo{ Char = new WinAPI.CharUnion{ UnicodeChar = 'C' } }
+            };
+
+            // 1 Tall, 2 Wide, 1 offset-x, 0 offset-y
+            ScreenSegment segment = new ScreenSegment(charArray, charArray.Length, 1, 2, 1, 0);
+
+            // Antag den kaster en exception siden 2 er udenfor.
+            Assert.ThrowsException<IndexOutOfRangeException>(() => segment[2, 0]);
+        }
+    }
+}
--- ParticleEngine.Tests/PhysicsLogicTests.cs
+++ ParticleEngine.Tests/PhysicsLogicTests.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Numerics;
+using ParticleEngine.Particles;
 
 namespace ParticleEngine.Tests
 {
@@ -13,13 +14,62 @@ namespace ParticleEngine.Tests
         [TestMethod]
         public void TestFreeFallingSand2TimesInARow()
         {
-            Physics.Instantiate<Particles.Sand>(new Vector2(0, 0));
-            Physics.Update();
+            // Setup
+            // Fordi det er en statisk klasse vil ændringer blive gemt gennem tests. Derfor kan der være rester fra en anden test.
+            ParticleGroup sandGroup = Physics.GetParticleGroup<Sand>();
 
-            Assert.AreEqual(new Vector2(0, 1), Physics.GetParticleGroup<Particles.Sand>().Particles[0]);
+            int sandID = 0;
+            if (!(sandGroup is null))
+            {
+                sandID = sandGroup.Particles.Count;
+            }
+            Physics.Instantiate<Sand>(new Vector2(0, 0));
+
+            sandGroup = Physics.GetParticleGroup<Sand>();
+
+            // Logic
+            Physics.Update();
+            Assert.AreEqual(new Vector2(0, 1), sandGroup.Particles[sandID]);
 
             Physics.Update();
-            Assert.AreEqual(new Vector2(0, 2), Physics.GetParticleGroup<Particles.Sand>().Particles[0]);
+            Assert.AreEqual(new Vector2(0, 2), sandGroup.Particles[sandID]);
+        }
+
+        [TestMethod]
+        public void TestSandFallingOnBlock()
+        {
+            // Setup
+            // Fordi det er en statisk klasse vil ændringer blive gemt gennem tests. Derfor kan der være rester fra en anden test.
+            ParticleGroup blockGroup = Physics.GetParticleGroup<Block>();
+            ParticleGroup sandGroup = Physics.GetParticleGroup<Sand>();
+
+            int blockID = 0, sandID = 0;
+            if (!(blockGroup is null))
+            {
+                blockID = blockGroup.Particles.Count;
+            }
+            if (!(sandGroup is null))
+            {
+                sandID = sandGroup.Particles.Count;
+            }
+
+            Physics.Instantiate<Block>(new Vector2(0, 2));
+            Physics.Instantiate<Sand>(new Vector2(0, 0));
+
+            blockGroup = Physics.GetParticleGroup<Block>();
+            sandGroup = Physics.GetParticleGroup<Sand>();
+
+            // Logic
+            Physics.Update();
+            // Antag at blokken ikke har rykket sig.
+            Assert.AreEqual(new Vector2(0, 2), blockGroup.Particles[blockID]);
+
+            // Antag at sand er flyttet.
+            Assert.AreEqual(new Vector2(0, 1), sandGroup.Particles[sandID]);
+
+            Physics.Update();
+            // Antag at sand bliver stående, siden blokken er i vejen.
+            Assert.AreEqual(new Vector2(0, 1), sandGroup.Particles[sandID]);
         }
     }
 }
